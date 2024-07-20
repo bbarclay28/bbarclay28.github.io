9e0059bebeba67ce1805f206a789277d707b5dbe
bb7882 <bbarclay28@outlook.com>
2024-07-20 17:33:25 -0400

    restart

--- bbarclay28/src/app/app.routes.ts
+++ bbarclay28/src/app/app.routes.ts
@@ -1,12 +1,3 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { Routes } from '@angular/router';
 
-export const routes: Routes = [{ path: '', component: HomeComponent}];
-
-@NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
-  })
-  export class AppRoutingModule { }
-  
+export const routes: Routes = [];
